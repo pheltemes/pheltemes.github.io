31977798ff364dfd8bcf4acd568e4a566fdf90f1
pheltemes <45610016+pheltemes@users.noreply.github.com>
2019-07-14 12:56:00 -0500

    Somes changes

---
+++ skills.js
@@ -0,0 +1,48 @@
+﻿function pythonSkill() {
+  document.getElementById("Python").innerHTML = "Proficiency: Advanced";
+}
+function pythonSkill2() {
+  document.getElementById("Python").innerHTML = "Python";
+}
+function rSkill() {
+  document.getElementById("R").innerHTML = "Proficiency: Advanced";
+}
+function rSkill2() {
+  document.getElementById("R").innerHTML = "R";
+}
+function cppSkill() {
+  document.getElementById("Cpp").innerHTML = "Proficiency: Expert";
+}
+function cppSkill2() {
+  document.getElementById("Cpp").innerHTML = "C++";
+}
+function htmlSkill() {
+  document.getElementById("Html").innerHTML = "Proficiency: Advanced";
+}
+function htmlSkill2() {
+  document.getElementById("Html").innerHTML = "HTML";
+}
+function cssSkill() {
+  document.getElementById("Css").innerHTML = "Proficiency: Advanced";
+}
+function cssSkill2() {
+  document.getElementById("Css").innerHTML = "CSS";
+}
+function jsSkill() {
+  document.getElementById("Js").innerHTML = "Proficiency: Advanced";
+}
+function jsSkill2() {
+  document.getElementById("Js").innerHTML = "Javascript";
+}
+function utcSkill() {
+  document.getElementById("Utc").innerHTML = "Proficiency: Advanced";
+}
+function utcSkill2() {
+  document.getElementById("Utc").innerHTML = "Unix Commands";
+}
+function kotSkill() {
+  document.getElementById("Kot").innerHTML = "Proficiency: Basic";
+}
+function kotSkill2() {
+  document.getElementById("Kot").innerHTML = "Kotlin";
+}
